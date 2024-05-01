5e9a46c1c5abcfbe842c5c8bf19026375dba141b
Jacob Graff <jacob@giphy.com>
2024-05-01 12:39:53 -0400

    maybe this will help

--- hasher-matcher-actioner/src/OpenMediaMatch/tests/omm_config.py
+++ hasher-matcher-actioner/src/OpenMediaMatch/tests/omm_config.py
@@ -11,7 +11,7 @@ PRODUCTION = False
 DBUSER = "media_match"
 DBPASS = "hunter2"
 DBHOST = "db"
-DBNAME = "media_match_test"
+DBNAME = "media_match"
 DATABASE_URI = f"postgresql+psycopg2://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}"
 
 ROLE_HASHER = True
